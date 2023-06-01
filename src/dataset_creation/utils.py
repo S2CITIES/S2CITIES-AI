5c393d2fa574973afd0b7f3bf47a94eb4ecf97dd
teobucci <teobucci8@gmail.com>
2023-06-01 23:14:34 +0200

    Sort videos

--- src/dataset_creation/utils.py
+++ src/dataset_creation/utils.py
@@ -12,6 +12,8 @@ def get_video_files(folder_path, video_extensions=('.mp4', '.mov')):
     folder_path = Path(folder_path)
     video_files = [file.name for file in folder_path.iterdir()
                    if file.is_file() and file.name.lower().endswith(video_extensions)]
+    # Sort the list of video files alphabetically
+    video_files.sort()
     return video_files
 
 def move_file(source: str, destination: str) -> None:
