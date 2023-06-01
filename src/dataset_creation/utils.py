27e7551bc3a753ade29378202f6df74b882ecabc
teobucci <teobucci8@gmail.com>
2023-06-01 23:05:41 +0200

    Update utils.py

--- src/dataset_creation/utils.py
+++ src/dataset_creation/utils.py
@@ -1,10 +1,21 @@
+"""
+Utility functions for file management.
+"""
+
 from pathlib import Path
+import os
 
 def get_video_files(folder_path, video_extensions=('.mp4', '.mov')):
     """
     Returns a list of all files with the specified video extensions, regardless of case, in the specified folder.
     """
     folder_path = Path(folder_path)
-    video_files = [file for file in folder_path.iterdir() 
+    video_files = [file.name for file in folder_path.iterdir()
                    if file.is_file() and file.name.lower().endswith(video_extensions)]
     return video_files
+
+def move_file(source: str, destination: str) -> None:
+    """
+    Moves a file from source to destination.
+    """
+    os.rename(source, destination)
