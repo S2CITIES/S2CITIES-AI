ac7680e9a315c0f0f954373df79fb40a666bbe86
pestrstr <giuseppestracquadanio2@gmail.com>
2023-06-09 16:31:41 +0200

    adding dropout regularization to c3d model

--- models/c3d.py
+++ models/c3d.py
@@ -21,38 +21,39 @@ class C3D(nn.Module):
         self.h = height
         self.w = width
         self.d = tempdepth
-        self.conv1 = nn.Conv3d(in_channels=self.c, out_channels=16, kernel_size=(self.d, 3, 3), padding=1)
-        self.conv2 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=(self.d, 3, 3), padding=1)
-        self.conv3 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(self.d, 3, 3), padding=1)
-        self.conv4 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=(self.d, 3, 3), padding=1)
-        self.conv5 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=(self.d, 3, 3), padding=1)
+        self.conv1 = nn.Conv3d(in_channels=self.c, out_channels=8, kernel_size=(self.d, 3, 3), padding=1)
+        self.conv2 = nn.Conv3d(in_channels=8, out_channels=16, kernel_size=(self.d, 3, 3), padding=1)
+        self.conv3 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=(self.d, 3, 3), padding=1)
+        self.conv4 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(self.d, 3, 3), padding=1)
+        # self.conv5 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=(self.d, 3, 3), padding=1)
         self.pool1 = nn.MaxPool3d(kernel_size=(1,2,2))
         self.pool = nn.MaxPool3d(kernel_size=(2,2,2))
-        self.fc = nn.Linear(in_features=64*(self.l//16)*(self.h//32)*(self.w//32), out_features=outputs)
+        self.fc = nn.Linear(in_features=32*(self.l//8)*(self.h//16)*(self.w//16), out_features=outputs)
+        self.dropout = nn.Dropout3d(p=0.5)
 
     def forward(self, x):
         # input shape (N, c, l, h, w)
-        out1 = self.pool1(self.conv1(x))
+        out1 = self.dropout(torch.relu(self.pool1(self.conv1(x))))
         # print(out1.shape)
         # out1 shape (N, 64, l, h//2, w//2)
-        out2 = self.pool(self.conv2(out1))
+        out2 = self.dropout(torch.relu(self.pool(self.conv2(out1))))
         # print(out2.shape)
         # out2 shape (N, 128, l//2, h//4, w//4)
-        out3 = self.pool(self.conv3(out2))
+        out3 = self.dropout(torch.relu(self.pool(self.conv3(out2))))
         # print(out3.shape)
         # out3 shape (N, 256, l//4, h//8, w//8)
-        out4 = self.pool(self.conv4(out3))
+        out4 = self.dropout(torch.relu(self.pool(self.conv4(out3))))
         # print(out4.shape)
         # out4 shape (N, 256, l//8, h//16, w//16)
-        out5 = self.pool(self.conv4(out4))
+        # out5 = self.pool(self.conv4(out4))
         # print(out5.shape)
         # out5 shape (N, 256, l//16, h//32, w//32)
 
         if len(x.shape) == 5:
             # Batched input, consider this when flattening
-            out = torch.flatten(out5, start_dim=1)
+            out = torch.flatten(out4, start_dim=1)
         else:
-            out = torch.flatten(out5)
+            out = torch.flatten(out4)
 
         # print(out.shape)
         return self.fc(out) # Output of linear layer before Softmax activation. The computation of the pdf (softmax) is embedded in PyTorch (nn.CrossEntropyLoss) 
@@ -64,6 +65,7 @@ if __name__ == '__main__':
     # Every video is reshaped with a frame shape (60, 80) (initial resolution is downscaled by a factor of 4)
     x = torch.randn((2, 3, 80, 60, 80))
     model = C3D(channels=3, length=80, height=60, width=80, tempdepth=3, outputs=25)
+    print(f"Total parameters: {sum(p.numel() for p in model.parameters())}")
     out = model(x)
     print(out.shape) # Expected torch.Size([2, 25])
 
