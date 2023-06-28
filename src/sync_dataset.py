629d3431c7b415530b558e8e60a4575ea9da1ef0
teobucci <teobucci8@gmail.com>
2023-06-28 17:44:57 +0200

    Update sync_dataset.py

--- src/sync_dataset.py
+++ src/sync_dataset.py
@@ -2,6 +2,8 @@ import argparse
 import boto3
 import os
 import re
+import shutil
+import io
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -114,108 +116,104 @@ class DatasetSyncGoogleDrive:
         self.drive_service = build('drive', 'v3', credentials=self.credentials)
 
     def push_dataset(self):
-        # Retrieve the list of objects in the Google Drive folder
-        query = f"'{self.folder_id}' in parents and trashed = false"
-        response = self.drive_service.files().list(q=query, fields='nextPageToken, files(id, name, mimeType)').execute()
-        existing_files = {file['name']: file['id'] for file in response.get('files', [])}
+        pass
 
-        # Walk through the local directory
+    # Function to download a file from Google Drive
+    def download_file(self, file_id, destination_path):
+
+        if os.path.exists(destination_path):
+            print('File already exists:', destination_path)
+            return
+
+        request = self.drive_service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+
+        while done is False:
+            status, done = downloader.next_chunk()
+
+        fh.seek(0)
+        with open(destination_path, 'wb') as f:
+            shutil.copyfileobj(fh, f)
+
+        print('File downloaded successfully.')
+
+
+    # Function to recursively download files within a folder and its subfolders
+    def download_folder(self, folder_id, destination_folder):
+        results = self.drive_service.files().list(q=f"'{folder_id}' in parents and trashed=false", fields='files(id, name, mimeType)').execute()
+        files = results.get('files', [])
+
+        if not files:
+            print('No files found in the folder.')
+        else:
+            for file in files:
+                file_name = file['name']
+                file_id = file['id']
+                file_mime_type = file['mimeType']
+
+                # If it's a file, download it
+                if file_mime_type != 'application/vnd.google-apps.folder':
+                    destination_path = os.path.join(destination_folder, file_name)
+                    self.download_file(file_id, destination_path)
+                    self.full_paths.append(os.path.relpath(destination_path, args.path))
+                    print('Downloaded file:', file_name)
+
+                # If it's a folder, create a subfolder and recursively download files within it
+                else:
+                    print('Folder ID:', file_id)
+                    subfolder_path = os.path.join(destination_folder, file_name)
+                    os.makedirs(subfolder_path, exist_ok=True)
+                    self.download_folder(file_id, subfolder_path)
+
+
+    def pull_dataset(self):
+
+        self.full_paths = []
+
+        # Create the local folder if it doesn't exist
+        os.makedirs(self.local_path, exist_ok=True)
+        
+        # Download files from Google Drive not present locally
+        self.download_folder(self.folder_id, self.local_path)
+
+        # Delete any files in local that are not present in Google Drive
         for root, dirs, files in os.walk(self.local_path):
             for file in files:
                 local_file_path = os.path.join(root, file)
-                file_name = os.path.basename(local_file_path)
+                current_path = os.path.join(os.path.relpath(local_file_path, self.local_path))
 
                 # Check if the file matches any of the ignore patterns
-                if any(re.search(pattern, file_name) for pattern in self.ignore_patterns):
+                if any(re.search(pattern, file) for pattern in ignore_patterns):
                     print(f'Skipping {local_file_path}, matches ignore pattern')
                     continue
 
-                # Check if the file already exists in the Google Drive folder
-                if file_name in existing_files:
-                    print(f'Skipping {local_file_path}, already exists in Google Drive')
-                    existing_files.pop(file_name)  # Remove from existing files list
-                    continue
+                # Delete the file if it's not present in Google Drive
+                if current_path not in self.full_paths:
+                    # os.remove(local_file_path)
+                    print(f'Deleted {local_file_path} from local directory')
 
-                # Upload the file to Google Drive
-                file_metadata = {'name': file_name, 'parents': [self.folder_id]}
-                media = MediaFileUpload(local_file_path, resumable=True)
-                file = self.drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-                print(f'Uploaded {local_file_path} to Google Drive: {file.get("id")}')
-
-        # Delete any files in Google Drive that are not present in the local directory
-        for file_id in existing_files.values():
-            try:
-                self.drive_service.files().delete(fileId=file_id).execute()
-                print(f'Deleted {file_id} from Google Drive')
-            except HttpError as error:
-                print(f'An error occurred: {error}')
-
-    def pull_dataset(self):
-        # Retrieve the list of objects in the Google Drive folder
-        query = f"'{self.folder_id}' in parents and trashed = false"
-        response = self.drive_service.files().list(q=query, fields='nextPageToken, files(id, name)').execute()
-        #drive_files = {file['name']: file['id'] for file in response.get('files', [])}
-        items = response.get('files', [])
-        print(self.drive_service.files())
-        return
-        
-
-        # Walk through the local directory
-        for file_name, file_id in drive_files.items():
-            local_file_path = os.path.join(self.local_path, file_name)
-
-            # Skip if the file already exists locally
-            if os.path.exists(local_file_path):
-                print(f'Skipping {file_name}, already exists locally')
-                continue
-
-            # Download the file from Google Drive
-            request = self.drive_service.files().get_media(fileId=file_id)
-            file = BytesIO()
-            downloader = MediaIoBaseDownload(file, request)
-            done = False
-            while done is False:
-                status, done = downloader.next_chunk()
-                print(f'Download {int(status.progress() * 100)}.')
-            file.seek(0)
-
-            with open(local_file_path, 'wb') as f:
-                f.write(file.read())
-            print(f'Downloaded {file_name} from Google Drive: {local_file_path}')
-
-        # # Delete any files in local that are not present in Google Drive folder
-        # for root, dirs, files in os.walk(self.local_path):
-        #     for file in files:
-        #         local_file_path = os.path.join(root, file)
-
-        #         # Check if the file matches any of the ignore patterns
-        #         if any(re.search(pattern, file) for pattern in self.ignore_patterns):
-        #             print(f'Skipping {local_file_path}, matches ignore pattern')
-        #             continue
-
-        #         # Delete the file if it's not present in Google Drive
-        #         if file not in drive_files:
-        #             os.remove(local_file_path)
-        #             print(f'Deleted {local_file_path} from local directory')
+        print('Synchronization complete.')
 
 if __name__ == '__main__':
     
-    ignore_patterns = ['\.txt$', '\.log$', '\.DS_Store$']
+    ignore_patterns = ['\.txt$', '\.log$', '\.DS_Store$', '\.py$', '\.pyc$', '\.sh$', '\.json$']
 
     ####### GOOGLE DRIVE #######
 
     parser = argparse.ArgumentParser(description='Sync or download dataset from Google Drive')
-    parser.add_argument('-cred', '--credentials', type=str, required=True, help='Path to credentials JSON file')
-    parser.add_argument('-mode', '--mode', type=str, required=True, choices=['PUSH', 'PULL'], help='Sync mode')
-    parser.add_argument('-path', '--local_dataset_path', type=str, required=True, help='Local dataset path')
-    parser.add_argument('-folder', '--folder_id', type=str, required=True, help='Google Drive folder ID')
+    parser.add_argument('-cred', '--credentials', type=str, required=True, help='Path to credentials JSON file', dest='credentials')
+    parser.add_argument('-mode', '--mode', type=str, required=True, choices=['PUSH', 'PULL'], help='Sync mode', dest='mode')
+    parser.add_argument('-path', '--local_dataset_path', type=str, required=True, help='Local dataset path', dest='path')
+    parser.add_argument('-folder', '--folder_id', type=str, required=True, help='Google Drive folder ID', dest='folder_id')
     args = parser.parse_args()
 
     credentials = service_account.Credentials.from_service_account_file(args.credentials)
 
-    sync = DatasetSyncGoogleDrive(args.local_dataset_path, args.folder_id, ignore_patterns=ignore_patterns, credentials=credentials)
+    sync = DatasetSyncGoogleDrive(args.path, args.folder_id, ignore_patterns=ignore_patterns, credentials=credentials)
 
-    # python src/sync_dataset.py -cred cred.json -folder 1StzxTYaPJBFqlxfXQhdxbcgfKdotaxoF -path "data/s3-bucket" -mode PULL
+    # python src/sync_dataset.py -cred client_secret.json -folder 1StzxTYaPJBFqlxfXQhdxbcgfKdotaxoF -path "data/GDrive" -mode PULL
 
     
     ####### AWS S3 #######
