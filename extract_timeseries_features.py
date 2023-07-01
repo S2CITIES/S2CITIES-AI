146310632a06bd1a41ed36ceaee9e576ca04f8e6
teobucci <teobucci8@gmail.com>
2023-07-01 11:52:04 +0200

    Indent

--- extract_timeseries_features.py
+++ extract_timeseries_features.py
@@ -11,62 +11,68 @@ import pandas as pd
 
 from tsfresh import extract_features
 from tsfresh.utilities.dataframe_functions import impute
-from tsfresh.feature_extraction import ComprehensiveFCParameters, EfficientFCParameters, MinimalFCParameters
+from tsfresh.feature_extraction import (
+    ComprehensiveFCParameters,
+    EfficientFCParameters,
+    MinimalFCParameters,
+)
 
-# Read from json file
-with open("./src/const.json", "r", encoding="utf-8") as f:
-    const = json.load(f)
+if __name__ == "__main__":
+    # Read from json file
+    with open("./src/const.json", "r", encoding="utf-8") as f:
+        const = json.load(f)
 
-data_path = Path(const["DATA_PATH"])
-features_extracted_path = Path(const["FEATURES_EXTRACTED"])
+    data_path = Path(const["DATA_PATH"])
+    features_extracted_path = Path(const["FEATURES_EXTRACTED"])
 
-# Define empty lists
-list_0 = []
-list_1 = []
-y = []
+    # Define empty lists
+    list_0 = []
+    list_1 = []
+    y = []
 
-# Read npy files and append to lists
-for file in (data_path / features_extracted_path).glob('0/*.npy'):
-    data = np.load(file)
-    data_df = pd.DataFrame(data)
-    list_0.append(data_df)
-    y.append(0)
-        
-for file in (data_path / features_extracted_path).glob('1/*.npy'):
-    data = np.load(file)
-    data_df = pd.DataFrame(data)
-    list_1.append(data_df)
-    y.append(1)
+    # Read npy files and append to lists
+    for file in (data_path / features_extracted_path).glob("0/*.npy"):
+        data = np.load(file)
+        data_df = pd.DataFrame(data)
+        list_0.append(data_df)
+        y.append(0)
 
-# Convert lists to dataframes
-id = 0
-df_list = []
-for i, data_list in enumerate([list_0, list_1]):
-    for j, data_df in enumerate(data_list):
-        data_df['target'] = i
-        data_df['id'] = id
-        id += 1
-        df_list.append(data_df)
+    for file in (data_path / features_extracted_path).glob("1/*.npy"):
+        data = np.load(file)
+        data_df = pd.DataFrame(data)
+        list_1.append(data_df)
+        y.append(1)
 
-df = pd.concat(df_list, ignore_index=True)
+    # Convert lists to dataframes
+    id = 0
+    df_list = []
+    for i, data_list in enumerate([list_0, list_1]):
+        for j, data_df in enumerate(data_list):
+            data_df["target"] = i
+            data_df["id"] = id
+            id += 1
+            df_list.append(data_df)
 
-# Plot the time series
-# df[df['id'] == 3].drop(['id','target'],axis=1).plot(subplots=False, sharex=True, figsize=(10,10))
+    df = pd.concat(df_list, ignore_index=True)
 
-# Extract features using tsfresh
-# extraction_settings = MinimalFCParameters()
-X = extract_features(df.drop('target', axis=1), column_id='id', column_sort=None,
-                     # default_fc_parameters=extraction_settings,
-                     # we impute = remove all NaN features automatically
-                     impute_function=impute,
-                     n_jobs=1)
+    # Plot the time series
+    # df[df['id'] == 3].drop(['id','target'],axis=1).plot(subplots=False, sharex=True, figsize=(10,10))
 
-y = pd.Series(y)
+    # Extract features using tsfresh
+    X = extract_features(
+        df.drop("target", axis=1),
+        column_id="id",
+        column_sort=None,
+        impute_function=impute,  # we impute = remove all NaN features automatically
+        n_jobs=0,  # Doesn't work with n_jobs=1,2,3,4 https://stackoverflow.com/a/62655746
+    )
 
-# Create output directory
-output_path = data_path / const["TIMESERIES_FEATURES_EXTRACTED"]
-output_path.mkdir(parents=True, exist_ok=True)
+    y = pd.Series(y)
 
-# Save X and y as pickle files
-X.to_pickle(output_path / "X.pkl")
-y.to_pickle(output_path / "y.pkl")
+    # Create output directory
+    output_path = data_path / const["TIMESERIES_FEATURES_EXTRACTED"]
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    # Save X and y as pickle files
+    X.to_pickle(output_path / "X.pkl")
+    y.to_pickle(output_path / "y.pkl")
