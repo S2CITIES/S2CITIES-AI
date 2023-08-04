2758b70e2aba00397f9b60eb0b3b0a74fde7c543
teobucci <teobucci8@gmail.com>
2023-08-04 21:42:40 +0200

    update to set starting idx with args and use constants.py

--- dataset_creation_move_and_split.py
+++ dataset_creation_move_and_split.py
@@ -2,27 +2,26 @@
 The following is for being used when creating the dataset clips form the collected videos.
 """
 
-from pathlib import Path
-import os
-import json
-
+import argparse
 from src.dataset_creation.videoprocessor import VideoProcessor
+from src import constants
 
-# Read from json file
-with open("./src/const.json", "r", encoding="utf-8") as f:
-    const = json.load(f)
+parser = argparse.ArgumentParser()
+parser.add_argument('--starting_idx', type=int,
+                    help='Starting index for next video to be processed.')
+args = parser.parse_args()
 
 processor = VideoProcessor(
-    videos_arrived_folder=const["VIDEOS_ARRIVED"],
-    videos_raw_folder=const["VIDEOS_RAW"],
-    videos_raw_processed_folder=const["VIDEOS_RAW_PROCESSED"],
-    videos_splitted_folder=const["VIDEOS_SPLITTED"],
-    videos_labeled_folder=const["VIDEOS_LABELED"],
-    video_extensions=const["VIDEO_EXTENSIONS"],
-    subclip_duration=2.5,
-    shift_duration=1,
-    starting_idx=275
-    )
+    videos_arrived_folder=constants.VIDEOS_ARRIVED,
+    videos_raw_folder=constants.VIDEOS_RAW,
+    videos_raw_processed_folder=constants.VIDEOS_RAW_PROCESSED,
+    videos_splitted_folder=constants.VIDEOS_SPLITTED,
+    videos_labeled_folder=constants.VIDEOS_LABELED,
+    video_extensions=constants.VIDEO_EXTENSIONS,
+    subclip_duration=constants.SUBCLIP_DURATION,
+    shift_duration=constants.SHIFT_DURATION,
+    starting_idx=args.starting_idx,
+)
 
 processor.move_arrived_videos()
 processor.split_raw_videos()
