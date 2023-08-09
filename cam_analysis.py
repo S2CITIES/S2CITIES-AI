5ff9e1a8b7a648c91f9b47f2b5876a89917b0ea5
Cavalli98 <dario1.cavalli@mail.polimi.it>
2023-08-10 03:05:41 +0900

    Update cam_analysis.py

--- cam_analysis.py
+++ cam_analysis.py
@@ -74,7 +74,9 @@ def test(loader, model, criterion, device, epoch=None):
             y_preds = y_preds.detach().cpu()
             y_pred.append(y_preds)
 
-            if save and labels[0]=="1":
+            print(type(labels))
+            print(type(labels[0]))
+            if save and labels[0]==1:
                 inp = inp.to(torch.device("cpu"))
                 global input
                 input = inp
