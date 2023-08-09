54fdd1d52ad6edfb4e326239640b817730b2201a
Cavalli98 <dario1.cavalli@mail.polimi.it>
2023-08-10 04:13:03 +0900

    Update cam_analysis.py

--- cam_analysis.py
+++ cam_analysis.py
@@ -193,7 +193,9 @@ if __name__ == '__main__':
     cam_model.module.checked = True
 
     #print(cam_model)
-    print(cam_model.layer[-1].weight)
+    print(cam_model.module.get_submodule('classifier').layer[0].weight)
+    print(cam_model.module.get_submodule('classifier').layer[1].weight)
+    print(cam_model.module.get_submodule('classifier').layer[2].weight)
 
     val_accuracy, val_loss = test(loader=val_dataloader, model=cam_model, criterion=criterion, device=device, epoch=None)
 
