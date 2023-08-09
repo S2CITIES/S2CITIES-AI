1e0cd4bdba961e0c99b01625b5e4de0d3e40c725
Cavalli98 <dario1.cavalli@mail.polimi.it>
2023-08-10 00:12:31 +0900

    Update cam_analysis.py

--- cam_analysis.py
+++ cam_analysis.py
@@ -159,6 +159,6 @@ if __name__ == '__main__':
     # Load the best checkpoint obtained until now
     best_checkpoint=torch.load(os.path.join(args.model_save_path, f'best_model_{args.exp}.h5'))
     cam_model.load_state_dict(best_checkpoint)
+    cam_model.checked = True
 
     val_accuracy, val_loss = test(loader=val_dataloader, model=cam_model, criterion=criterion, device=device, epoch=None)
-    cam_model.animation
