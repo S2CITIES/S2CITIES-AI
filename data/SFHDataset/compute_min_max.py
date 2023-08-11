d1d1ba843c83cce3096d3eb86d08fe6871b4a366
Cavalli98 <dario1.cavalli@mail.polimi.it>
2023-08-11 18:05:51 +0900

    Update compute_min_max.py

--- data/SFHDataset/compute_min_max.py
+++ data/SFHDataset/compute_min_max.py
@@ -35,8 +35,8 @@ def get_SFH_min_max(target_dataset, image_size=112, norm_value=1.0, force_comput
             mx = torch.amax(video, dim=(1, 2, 3))
             mn = torch.amin(video, dim=(1, 2, 3))
             print(channel_max)
-            print(mn)
-            for c in range(len(mx)):
+            print(mx)
+            for c in range(mx.shape[0]):
                 if channel_max[c]<mx[c]:
                     channel_max = mx[c]
                 if channel_min[c]>mn[c]:
