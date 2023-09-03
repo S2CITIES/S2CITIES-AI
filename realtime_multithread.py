293473b9ae8f61a96f145eda3d3e8ee10f68f6cd
teobucci <teobucci8@gmail.com>
2023-09-03 22:34:12 +0200

    Format file

--- realtime_multithread.py
+++ realtime_multithread.py
@@ -17,22 +17,20 @@ import threading
 import time
 
 import cv2
-import joblib
 import mediapipe as mp
 import numpy as np
 
 from models.model_mpkpts import Model
-from src.keypointsextractor import KeypointsExtractor
 
 # Set up the feature extractor
-#feature_extractor = FeatureExtractor()
+# feature_extractor = FeatureExtractor()
+
 
 # TODO
 # limitazioni:
 # - sto considerando una sola mano
 # - e che sia continua (ma ok)
 def thread_extract_keypoints():
-
     # Set up the global variables
     global timeseries
 
@@ -44,20 +42,19 @@ def thread_extract_keypoints():
     mp_hands = mp.solutions.hands
 
     # Set up the hyperparameters
-    interval = 1                   # in seconds
-    window = 2.5                   # in seconds
-    prev = 0                       # in seconds
+    interval = 1  # in seconds
+    window = 2.5  # in seconds
+    prev = 0  # in seconds
     frames_to_next_prediction = 0  # in frames
-    frame_rate = 12                # in frames per second
+    frame_rate = 12  # in frames per second
 
     while True:
-
         # Read a frame from the video capture
         ret, frame = cap.read()
 
         # Implement logit to limit the frame rate to frame_rate
         time_elapsed = time.time() - prev
-        if not time_elapsed > 1./frame_rate:
+        if not time_elapsed > 1.0 / frame_rate:
             continue
         prev = time.time()
 
@@ -68,15 +65,21 @@ def thread_extract_keypoints():
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # Detect the hand landmarks using Mediapipe
-        with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        with mp_hands.Hands(
+            min_detection_confidence=0.5, min_tracking_confidence=0.5
+        ) as hands:
             results = hands.process(frame_rgb)
 
             # If there is exactly one hand
             if results.multi_hand_landmarks and len(results.multi_hand_landmarks) == 1:
-
                 # Extract the keypoints from the hand landmarks
                 # TODO: implement the custom extractor
-                keypoints = np.array([[res.x, res.y, res.z] for res in results.multi_hand_landmarks[0].landmark]).flatten()
+                keypoints = np.array(
+                    [
+                        [res.x, res.y, res.z]
+                        for res in results.multi_hand_landmarks[0].landmark
+                    ]
+                ).flatten()
                 # If the timeseries is not full, add the keypoints to it
                 if len(timeseries) < frame_rate * window:
                     timeseries.append(keypoints)
@@ -113,18 +116,20 @@ def thread_extract_keypoints():
         # lunghezze minori da riempire con zeri o NAN
 
         # Print the length of the timeseries to the console for debugging
-        #print("Timeseries length:", len(timeseries))
+        # print("Timeseries length:", len(timeseries))
 
         # Draw the hand landmarks on the frame
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(
+                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
+                )
 
         # Show the frame
-        cv2.imshow('Hand Keypoints', frame)
+        cv2.imshow("Hand Keypoints", frame)
 
         # Check if the user has pressed the 'q' key to quit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             # Perform the last prediction so that the thread can stop
             predict_event.set()
 
@@ -137,16 +142,15 @@ def thread_extract_keypoints():
 
 
 def thread_predict(
-        stop_event,
-        predict_event,
-        training_results,
-        model_choice,
-        threshold,
-        tsfresh_parameters,
-        scaler,
-        final_features,
-        ):
-
+    stop_event,
+    predict_event,
+    training_results,
+    model_choice,
+    threshold,
+    tsfresh_parameters,
+    scaler,
+    final_features,
+):
     # Load the model
     model = Model(
         training_results=training_results,
@@ -154,51 +158,56 @@ def thread_predict(
         tsfresh_parameters=tsfresh_parameters,
         scaler=scaler,
         final_features=final_features,
-        )
+    )
 
     while not stop_event.is_set():
-
         predict_event.wait()
 
         # Predict the output using the model
         proba = model.predict(timeseries)
 
-        output = (proba[:,1] >= threshold).astype(bool)
+        output = (proba[:, 1] >= threshold).astype(bool)
 
         print(f"Predicted output: {output} [{proba[:,1]}]")
 
         # Reset the predict event
         predict_event.clear()
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     # Argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--training_results',
-                        type=str,
-                        required=True,
-                        help='Path to the training results file')
-    parser.add_argument('--tsfresh_parameters',
-                        type=str,
-                        required=True,
-                        help='Path to the tsfresh parameters file')
-    parser.add_argument('--scaler',
-                        type=str,
-                        required=True,
-                        help='Path to the scaler file')
-    parser.add_argument('--model_choice',
-                        type=str,
-                        choices=['RF', 'SVM', 'LR'],
-                        required=True,
-                        help='Name of the model to load')
-    parser.add_argument('--threshold',
-                        type=float,
-                        default=0.5,
-                        help='Threshold for the classification')
-    parser.add_argument('--final_features',
-                        type=str,
-                        required=True,
-                        help='Path to the final features file')
+    parser.add_argument(
+        "--training_results",
+        type=str,
+        required=True,
+        help="Path to the training results file",
+    )
+    parser.add_argument(
+        "--tsfresh_parameters",
+        type=str,
+        required=True,
+        help="Path to the tsfresh parameters file",
+    )
+    parser.add_argument(
+        "--scaler", type=str, required=True, help="Path to the scaler file"
+    )
+    parser.add_argument(
+        "--model_choice",
+        type=str,
+        choices=["RF", "SVM", "LR"],
+        required=True,
+        help="Name of the model to load",
+    )
+    parser.add_argument(
+        "--threshold", type=float, default=0.5, help="Threshold for the classification"
+    )
+    parser.add_argument(
+        "--final_features",
+        type=str,
+        required=True,
+        help="Path to the final features file",
+    )
     args = parser.parse_args()
 
     # Set up the timeseries
@@ -212,7 +221,7 @@ if __name__ == '__main__':
 
     # Start the predict thread
     predict_process = threading.Thread(
-        name='predict',
+        name="predict",
         target=thread_predict,
         args=(
             stop_event,
@@ -223,8 +232,8 @@ if __name__ == '__main__':
             args.tsfresh_parameters,
             args.scaler,
             args.final_features,
-            )
-        )
+        ),
+    )
     predict_process.start()
 
     # Start the extract keypoints thread which is the main thread
