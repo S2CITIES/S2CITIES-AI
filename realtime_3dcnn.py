4703e989aa963206015d9fbcabf40fe638edded3
teobucci <teobucci8@gmail.com>
2023-09-05 18:52:54 +0200

    Revert changes

--- realtime_3dcnn.py
+++ realtime_3dcnn.py
@@ -18,20 +18,16 @@ import threading
 import time
 
 import cv2
-#import mediapipe as mp
+import mediapipe as mp
 import numpy as np
 import torch
-import torch.nn.functional as F
 from PIL import Image
 
 import transforms.spatial_transforms as SPtransforms
-import transforms.temporal_transforms as TPtransforms
-#import torchvision.transforms as transforms
 from build_models import build_model
 
 
 def crop_frame(frame, height, width, aspect_ratio, target_ratio):
-
     if aspect_ratio > target_ratio:
         # Original video is wider, crop the sides
         target_width = int(height * target_ratio)
@@ -45,11 +41,10 @@ def crop_frame(frame, height, width, aspect_ratio, target_ratio):
         crop_bottom = height - target_height - crop_top
         crop_left, crop_right = 0, 0
 
-    return frame[crop_top:height - crop_bottom, crop_left:width - crop_right]
+    return frame[crop_top : height - crop_bottom, crop_left : width - crop_right]
 
 
 def thread_collect_frames(hand_detector=True):
-
     # Set up the global variables
     global frame_queue
 
@@ -66,16 +61,16 @@ def thread_collect_frames(hand_detector=True):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    aspect_ratio =  width / height
+    aspect_ratio = width / height
 
     print(f"Input video resolution: {(width, height)}")
 
     # Set up the hyperparameters
-    interval = 1                   # in seconds
-    window = 2.5                   # in seconds
-    prev = 0                       # in seconds
+    interval = 1  # in seconds
+    window = 2.5  # in seconds
+    prev = 0  # in seconds
     frames_to_next_prediction = 0  # in frames
-    frame_rate = 30                # in frames per second
+    frame_rate = 30  # in frames per second
 
     while True:
         # Read a frame from the video capture
@@ -83,7 +78,7 @@ def thread_collect_frames(hand_detector=True):
 
         # Implement logit to limit the frame rate to frame_rate
         time_elapsed = time.time() - prev
-        if not time_elapsed > 1./frame_rate:
+        if not time_elapsed > 1.0 / frame_rate:
             continue
         prev = time.time()
 
@@ -91,17 +86,38 @@ def thread_collect_frames(hand_detector=True):
         frame = cv2.flip(frame, 1)
 
         if not hand_detector:
-            converted_frame = crop_frame(frame, height=height, width=width, aspect_ratio=aspect_ratio, target_ratio=1)
+            converted_frame = crop_frame(
+                frame,
+                height=height,
+                width=width,
+                aspect_ratio=aspect_ratio,
+                target_ratio=1,
+            )
             converted_frame = Image.fromarray(converted_frame)
             frame_queue.append(converted_frame)
         else:
-            with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+            with mp_hands.Hands(
+                min_detection_confidence=0.5, min_tracking_confidence=0.5
+            ) as hands:
                 results = hands.process(frame)
                 # One hand (and exactly ONE hand) is detected
-                if results.multi_hand_landmarks and len(results.multi_hand_landmarks) == 1:
-                    mp_drawing.draw_landmarks(frame, results.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
+                if (
+                    results.multi_hand_landmarks
+                    and len(results.multi_hand_landmarks) == 1
+                ):
+                    mp_drawing.draw_landmarks(
+                        frame,
+                        results.multi_hand_landmarks[0],
+                        mp_hands.HAND_CONNECTIONS,
+                    )
                     # Convert the aspect ratio of the frame to a target ratio of 1 by cropping
-                    converted_frame = crop_frame(frame, height=height, width=width, aspect_ratio=aspect_ratio, target_ratio=1)
+                    converted_frame = crop_frame(
+                        frame,
+                        height=height,
+                        width=width,
+                        aspect_ratio=aspect_ratio,
+                        target_ratio=1,
+                    )
                     # Convert frame to PIL Image
                     converted_frame = Image.fromarray(converted_frame)
                     # Push the frame to the frame queue
@@ -129,7 +145,7 @@ def thread_collect_frames(hand_detector=True):
         # print("Showing the frame...")
         # cv2.imshow('frame', frame)
         # Check if the user has pressed the 'q' key to quit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             # Perform the last prediction so that the thread can stop
             predict_event.set()
             # Trigger the stop event that will stop the predict thread
@@ -140,11 +156,9 @@ def thread_collect_frames(hand_detector=True):
 
 
 def thread_predict(stop_event, predict_event, model, spatial_transform, threshold):
-
     global inference_steps
 
     while not stop_event.is_set():
-
         predict_event.wait()
         start_time = time.time()
 
@@ -152,7 +166,7 @@ def thread_predict(stop_event, predict_event, model, spatial_transform, threshol
         clip = frame_queue.copy()
 
         n_frames = len(clip)
-        downstream_factor = n_frames//16
+        downstream_factor = n_frames // 16
         selected_frames = list(range(0, n_frames, downstream_factor))
 
         clip = [clip[idx] for idx in selected_frames]
@@ -178,9 +192,11 @@ def thread_predict(stop_event, predict_event, model, spatial_transform, threshol
         # See what happens with tensor full of zeros or randn
         # clip = [torch.randn((3, 112, 112)) for _ in range(len(clip))]
 
-        clip_tensor = torch.stack(clip, dim=0) # Tensor with shape TCHW
-        clip_tensor = clip_tensor.permute(1, 0, 2, 3) # Tensor with shape CTHW
-        clip_tensor = torch.unsqueeze(clip_tensor, dim=0) # Add the batch dimension for inference
+        clip_tensor = torch.stack(clip, dim=0)  # Tensor with shape TCHW
+        clip_tensor = clip_tensor.permute(1, 0, 2, 3)  # Tensor with shape CTHW
+        clip_tensor = torch.unsqueeze(
+            clip_tensor, dim=0
+        )  # Add the batch dimension for inference
 
         with torch.no_grad():
             # Predict the output using the model
@@ -215,10 +231,13 @@ def thread_predict(stop_event, predict_event, model, spatial_transform, threshol
         print(f"Total inference time: {end_time-start_time}")
 
         codec = cv2.VideoWriter_fourcc(*"mp4v")  # Video codec (e.g., "mp4v", "XVID")
-        output_file = os.path.join('real_time_test', f'test_output_step{inference_steps}_result{result}_{torch.sigmoid(logits).item()}.mp4')  # Output video file name
+        output_file = os.path.join(
+            "real_time_test",
+            f"test_output_step{inference_steps}_result{result}_{torch.sigmoid(logits).item()}.mp4",
+        )  # Output video file name
         inference_steps += 1
         frame_size = (112, 112)  # Frame size (width, height)
-        fps = 16/2.5
+        fps = 16 / 2.5
 
         video_writer = cv2.VideoWriter(output_file, codec, fps, frame_size)
 
@@ -234,40 +253,44 @@ def thread_predict(stop_event, predict_event, model, spatial_transform, threshol
         # Reset the predict event
         predict_event.clear()
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     # Set up the frame queue (to push and pop frames)
     frame_queue = []
     inference_steps = 0
     print("Building gesture recognition model...")
-    model_type = 'mobilenetv2' # Make the model type an argument through argparse
-    model_path = 'checkpoints/best_model_mn2-dataset-no-temp-trans-no-norm255-size112-complete.h5'
+    model_type = "mobilenetv2"  # Make the model type an argument through argparse
+    model_path = "checkpoints/best_model_mn2-dataset-no-temp-trans-no-norm255-size112-complete.h5"
     threshold = 0.5
     # Load 3DCNN model
-    model = build_model(model_path=model_path,
-                        type=model_type,
-                       gpus=[0],            # Inference on a single GPU
-                       sample_size=112,
-                       output_features=1,   # Output will be sigmoid-activated
-                       num_classes=1,
-                       finetune=False,      # Load the entire model as it is, no fine-tuning
-                       state_dict=True)     # Only the state_dict of the model was saved
-    model.eval()                            # Set model to eval model
+    model = build_model(
+        model_path=model_path,
+        type=model_type,
+        gpus=[0],  # Inference on a single GPU
+        sample_size=112,
+        output_features=1,  # Output will be sigmoid-activated
+        num_classes=1,
+        finetune=False,  # Load the entire model as it is, no fine-tuning
+        state_dict=True,
+    )  # Only the state_dict of the model was saved
+    model.eval()  # Set model to eval model
 
     # Read mean and std. computed on the training set from info.json
     training_set = "SFH_Dataset_S2CITIES_test_new_negatives_ratio1_112x112_fps6.4"
 
-    with open('data/SFHDataset/info.json', 'r') as f:
-        info =json.load(f)
-    
-    mean = info[training_set]['mean']
-    std = info[training_set]['std']
+    with open("data/SFHDataset/info.json", "r") as f:
+        info = json.load(f)
 
-    spatial_transform = SPtransforms.Compose([
-        SPtransforms.Scale(size=112),
-        SPtransforms.ToTensor(norm_value=255),
-        # SPtransforms.Normalize(mean=mean, std=std)
-    ])
+    mean = info[training_set]["mean"]
+    std = info[training_set]["std"]
+
+    spatial_transform = SPtransforms.Compose(
+        [
+            SPtransforms.Scale(size=112),
+            SPtransforms.ToTensor(norm_value=255),
+            # SPtransforms.Normalize(mean=mean, std=std)
+        ]
+    )
 
     # Create the stop event
     stop_event = threading.Event()
@@ -276,7 +299,11 @@ if __name__ == '__main__':
     predict_event = threading.Event()
 
     # Start the predict thread
-    predict_process = threading.Thread(name='predict', target=thread_predict, args=(stop_event,predict_event, model, spatial_transform, threshold))
+    predict_process = threading.Thread(
+        name="predict",
+        target=thread_predict,
+        args=(stop_event, predict_event, model, spatial_transform, threshold),
+    )
     predict_process.start()
 
     # Start the extract keypoints thread which is the main thread
